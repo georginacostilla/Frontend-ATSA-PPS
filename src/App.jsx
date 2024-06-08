35b774857b5ee030003c60108c17c79ae2c2482a
georginacostilla <georginacostilla.gc@gmail.com>
2024-06-08 04:42:29 -0300

    se añade un estado global mediante context para disponibilizar los datos de los usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import UserContext from './context/UserContext';
 import Rutas from './components/routes/Rutas';
+import Footer from './components/footer/Footer';
 import '../src/App.css';
 
 function App() {
 
   return (
     <>
-      <Rutas />
+      <UserContext>
+        <Rutas />
+      </UserContext>
+      <Footer />
     </>
   )
 }
