b0daef10c2d9df419c7b5671cbd36cc72a83ab2e
Juan Lassalle <juanlassalle23@hotmail.com>
2024-06-09 21:04:22 -0300

    configuracion en el app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import UserContext from './context/UserContext';
+import Rutas from './components/routes/Rutas';
+import Footer from './components/footer/Footer';
 import '../src/App.css';
-import Footer from '../src/components/footer/Footer.jsx'
 
 function App() {
 
   return (
     <>
-      <Footer/>
+      <UserContext>
+        <Rutas />
+      </UserContext>
+      <Footer />
     </>
   )
 }
