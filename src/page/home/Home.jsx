588cd2ecd3627c1898ad8e441f08b6abf027cae5
georginacostilla <georginacostilla.gc@gmail.com>
2024-06-11 05:23:49 -0300

    se comienza a trabajar en el diseño de los botones del Home

---
+++ src/page/home/Home.jsx
@@ -0,0 +1,32 @@
+import Button from 'react-bootstrap/Button';
+
+const Home = () => {
+    return (
+        <>
+            <div className="d-flex justify-content-center align-items-center mt-5">
+                <div className="d-grid gap-2 w-25">
+                    <Button variant="primary" size="lg">
+                        Cursada
+                    </Button>
+                    <Button variant="primary" size="lg">
+                        Examenes
+                    </Button>
+                    <Button variant="primary" size="lg">
+                        Alumnos
+                    </Button>
+                    <Button variant="primary" size="lg">
+                        Cuotas
+                    </Button>
+                    <Button variant="primary" size="lg">
+                        Docentes
+                    </Button>
+                    <Button variant="primary" size="lg">
+                        Materias
+                    </Button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home;
