e514afe364633910dd4518400496618632b8272b
georginacostilla <georginacostilla.gc@gmail.com>
2024-06-18 23:10:42 -0300

    se añade archivo base

--- src/components/routes/Rutas.jsx
+++ src/components/routes/Rutas.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from '../../page/login/Login';
-import Register from '../../page/register/Register';
+import Register from '../../page/adminUsuario/Register';
 import Home from '../../page/home/Home';
 import Error404 from '../../page/error404/Error404';
 
---
+++ src/page/adminUsuario/AdminUsuarioModal.jsx
@@ -0,0 +1,11 @@
+
+
+const AdminUsuarioModal = () => {
+  return (
+    <div>AdminUsuarioModal</div>
+  )
+}
+
+export default AdminUsuarioModal
+
+// campos: Apellido y nombre en un solo campo, correo y contraseña
